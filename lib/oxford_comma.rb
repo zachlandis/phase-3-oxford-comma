4f48e5d04202f51450c4c2475d962a84dc67474c
Zach <zachlandis91@gmail.com>
2023-04-07 11:05:27 -0600

    commit tree f3374788a8e2c8aabdbb19624fdfed75c6642aee

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     if (array.length == 1)
+#         str1 = array.join(", ")
+#         str1
+#     elsif (array.length == 2)
+#         str2 = array.join(" and ")
+#         str2
+#     else (array.length > 2)
+#         str3 = array[-1] = "and #{array[-1]}"
+#         str3
+#     end
+# end
 
+
+def oxford_comma(array)
+    return array.join(" and ") if array.size < 3
+
+    array[-1] = "and #{array[-1]}"
+
+    array.join(", ")
 end
